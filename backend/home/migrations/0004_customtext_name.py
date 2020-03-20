1a4e7bdf81478e20802b452fc1bca579c02107f3
Crowdbotics <team@crowdbotics.com>
2020-03-20 07:11:35 +0000

    Update models

--- backend/home/migrations/0004_customtext_name.py
+++ backend/home/migrations/0004_customtext_name.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_test'),
+        ("home", "0003_test"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='name',
+            model_name="customtext",
+            name="name",
             field=models.BinaryField(blank=True, null=True),
         ),
     ]
